269a5ee7f84d1f91a96c00e57169fa17dda34370
Ehsan Darvishi <127976588+ehsandarvishi2003@users.noreply.github.com>
2024-10-28 20:11:14 +0330

    1403-08-07 Add TicketTime & DeveloperId property to ticket -  Add TicketTime & DeveloperId property in GetTicketMassagesHandler return value inside the ticketInfo -Add DeveloperId enum - ChangeDevelopedBy command for Ticket EndPoint (API)

---
+++ Ticketing/EndPoints/Ticket/Command/ChangeDevelopedBy/DevelopedByQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Ticketing.Domain.Enums;
+
+namespace Ticketing.EndPoints.Ticket.Command.ChangeDevelopedBy
+{
+    public class DevelopedByQuery:IRequest<object>
+    {
+        public string? TicketTime { get; set; }
+        public Developer DeveloperId { get; set; }
+        public int TicketId { get; set; }
+    }
+}
