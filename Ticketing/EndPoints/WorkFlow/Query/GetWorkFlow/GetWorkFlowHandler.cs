312f462b16d90dd4957c3594b1b85424c9da7e7e
[Ehsan-Darvishi] <ehsan20031203@gmail.com>
2024-11-19 17:00:07 +0330

    1403-08-29 : -Change GetWorkFlowHandler return (not complit)

--- Ticketing/EndPoints/WorkFlow/Query/GetWorkFlow/GetWorkFlowHandler.cs
+++ Ticketing/EndPoints/WorkFlow/Query/GetWorkFlow/GetWorkFlowHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using System.Globalization;
+using Ticketing.Application.Service.Stauts;
 using Ticketing.Domain.Contracts;
 using Ticketing.EndPoints.Ticket.Query.GetGroupTicketInfo;
 
@@ -7,21 +8,25 @@ namespace Ticketing.EndPoints.WorkFlow.Query.GetWorkFlow
 {
     public class GetWorkFlowHandler
     {
-        public class Handler(ITicketFlowService ticketFlowService, ILogger<GetRoleTicketInfoHandler> _logger) : IRequestHandler<GetWorkFlowQuery, object>
+        public class Handler(ITicketFlowService ticketFlowService, IStatusService statusService , ILogger<GetRoleTicketInfoHandler> _logger) : IRequestHandler<GetWorkFlowQuery, object>
         {
             public async Task<object> Handle(GetWorkFlowQuery query, CancellationToken cancellationToken)
             {
                 try
                 {
                     var ticketFlowList = await ticketFlowService.ListAsync(a => a.TicketId == query.TicketId);
+                    var liststatus = await statusService.ListAsync(null);
+
                     var p = new PersianCalendar();
                     return ticketFlowList.OrderByDescending(a => a.InsertDate).Select(x => new
                     {
+                        flowId = x.Id,
                         ticketId = x.TicketId,
                         userId = x.UserId,// Ticket belongs to this user
                         userName = x.Username,// Made by this user name
-                        statusId = x.StatusId,
-                        curentRoleId = x.CurrentRoleId,
+                        StatusId = liststatus.FirstOrDefault(a => a.Id == x.StatusId).Id,
+                        Status = liststatus.FirstOrDefault(a => a.Id == x.StatusId).Name,
+                        currentRoleId = x.CurrentRoleId,
                         previousRoleId = x.CurrentRoleId,
                         insertedDate = p.GetHour(x.InsertDate) + ":" +
                                       (p.GetMinute(x.InsertDate) < 10 ? ("0" + p.GetMinute(x.InsertDate)) : p.GetMinute(x.InsertDate)) + " - " +
